71567d5200461a83b6f14d2c320111679b37791f
Aleksandr232 <aleksrowing232@gmail.com>
2024-05-29 15:09:56 +0300

    api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-      </head>
       <body className={MontserratFamily.className}>
         <StoreProvider>
           {/* <Header /> */}
