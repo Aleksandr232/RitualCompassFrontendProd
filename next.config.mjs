b6ceb7bcbd416517610da2f9b4eed3ec005d2fa9
Aleksandr232 <aleksrowing232@gmail.com>
2024-05-29 15:26:17 +0300

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-const nextCors = require('next-cors');
-
 const nextConfig = {
   images: {
     domains: process.env.NEXT_PUBLIC_API_URL
@@ -13,28 +11,6 @@ const nextConfig = {
     });
     return config;
   },
-  /* Add CORS configuration */
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://www.ritualcompass.com',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
 };
 
-export default nextCors(nextConfig);
+export default nextConfig;
