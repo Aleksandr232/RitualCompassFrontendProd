3df6c75de68e7376b0176a25ef7ce94c256f4c52
Aleksandr232 <aleksrowing232@gmail.com>
2024-05-29 15:23:35 +0300

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,40 @@
+const nextCors = require('next-cors');
+
 const nextConfig = {
   images: {
-    domains: process.env.NEXT_PUBLIC_API_URL ? [process.env.NEXT_PUBLIC_API_URL.split(":")[0]] : [],// Добавьте это поле
-    /* domains: ['localhost'] */
+    domains: process.env.NEXT_PUBLIC_API_URL
+      ? [process.env.NEXT_PUBLIC_API_URL.split(':')[0]]
+      : [],
   },
   webpack(config) {
     config.module.rules.push({
       test: /.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
     return config;
   },
+  /* Add CORS configuration */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.ritualcompass.com',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+export default nextCors(nextConfig);
