9109d9b84c19ac587758371d120d40a181b6912f
Aleksandr232 <aleksrowing232@gmail.com>
2024-05-30 17:33:38 +0300

    navigation

--- src/Layout/components/Navigation/Navigation.tsx
+++ src/Layout/components/Navigation/Navigation.tsx
@@ -1,38 +1,85 @@
-import { FC } from "react";
+import { useState, FC } from "react";
 import styles from "./Navigation.module.css";
 import LinkTag from "@/components/LinkTag/LinkTag";
 import { routers } from "@/utils/routers";
 
 const Navigation: FC = () => {
-  return (
-    <ul className={styles.navigation_item}>
-      <li>
-        <LinkTag url={routers.home} appearance="ghost">
-          Главная
-        </LinkTag>
-      </li>
-      <li>
-        <LinkTag url={routers.cemetery} appearance="ghost">
-          Кладбища
-        </LinkTag>
-      </li>
-      <li>
-        <LinkTag url={routers.morgue} appearance="ghost">
-          Морги
-        </LinkTag>
-      </li>
-      <li>
-        <LinkTag url={routers.about} appearance="ghost">
-          О нас
-        </LinkTag>
-      </li>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      <li>
-        <LinkTag url={routers.home} appearance="ghost">
-          Контакты
-        </LinkTag>
-      </li>
-    </ul>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className={styles.navigation}>
+      <div className={styles.navigation_desktop}>
+        <ul className={styles.navigation_item}>
+          <li>
+            <LinkTag url={routers.home} appearance="ghost">
+              Главная
+            </LinkTag>
+          </li>
+          <li>
+            <LinkTag url={routers.cemetery} appearance="ghost">
+              Кладбища
+            </LinkTag>
+          </li>
+          <li>
+            <LinkTag url={routers.morgue} appearance="ghost">
+              Морги
+            </LinkTag>
+          </li>
+          <li>
+            <LinkTag url={routers.about} appearance="ghost">
+              О нас
+            </LinkTag>
+          </li>
+          <li>
+            <LinkTag url={routers.home} appearance="ghost">
+              Контакты
+            </LinkTag>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.navigation_mobile}>
+        <button className={styles.burger_menu} onClick={toggleMenu}>
+          <span className={styles.burger_line}></span>
+          <span className={styles.burger_line}></span>
+          <span className={styles.burger_line}></span>
+        </button>
+
+        {isMenuOpen && (
+          <ul className={styles.mobile_menu}>
+            <li>
+              <LinkTag url={routers.home} appearance="ghost">
+                Главная
+              </LinkTag>
+            </li>
+            <li>
+              <LinkTag url={routers.cemetery} appearance="ghost">
+                Кладбища
+              </LinkTag>
+            </li>
+            <li>
+              <LinkTag url={routers.morgue} appearance="ghost">
+                Морги
+              </LinkTag>
+            </li>
+            <li>
+              <LinkTag url={routers.about} appearance="ghost">
+                О нас
+              </LinkTag>
+            </li>
+            <li>
+              <LinkTag url={routers.home} appearance="ghost">
+                Контакты
+              </LinkTag>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
   );
 };
 
