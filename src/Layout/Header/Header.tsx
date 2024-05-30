033a17824447a59a87ecf4ff61bb61ae03f7baef
Aleksandr232 <aleksrowing232@gmail.com>
2024-05-31 00:15:37 +0300

    logo

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header: FC<IHeaderProps> = (props) => {
     <header className={styles.header} {...props}>
       <div className={styles.logo_wrapper}>
         {/* <Htag tag="h2">Оказание бесплатной ритуальной помощи в г.Казань</Htag> */}
-        <div style={{width: 40}}>
+        <div className={styles.logo}>
         <Logo/>
         </div>
         
