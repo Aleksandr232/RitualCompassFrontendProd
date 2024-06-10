38cc60fb1c8f53a1832636e69bb3f0426133288a
Aleksandr232 <aleksrowing232@gmail.com>
2024-06-10 09:49:20 +0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,12 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-      <link rel="icon"  href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="120x120" href="/favicon-120x120.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       <meta name="google-site-verification" content="qMkF8mo5iBwpHPxlXmL0mi098687P7dcsagc9TWvdvw" />
       <meta name="yandex-verification" content="714505a43000fc0f" />
       </head>
