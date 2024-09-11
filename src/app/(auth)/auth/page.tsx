7f49d96a68f5017bf3b6a98d618070c06fa9b236
Aleksandr232 <aleksrowing232@gmail.com>
2024-09-12 00:55:39 +0300

    google

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import GoogleSigninButton from "./google/GoogleSigninButton";
+import GoogleCallback from "./google/GoogleCallback";
 
 export default function GoogleAuth() {
   return (
     <div>
       <GoogleSigninButton />
+      <GoogleCallback/>
     </div>
   );
 }
